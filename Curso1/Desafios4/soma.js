7a1f0c2521921c3882c6ae4f2fc63fce4df961a0
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-17 19:12:38 -0300

    Adiciona subtração de dois valores e exibe o resultado

---
+++ Curso1/Desafios4/soma.js
@@ -0,0 +1,9 @@
+var valor1 = 10;
+var valor2 = 5;
+var resultado = valor1 + valor2;
+console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`);
+
+var valor1 = 10;
+var valor2 = 5;
+var resultado = valor1 - valor2;
+console.log(`A diferença entre ${valor1} e ${valor2} é igual a ${resultado}.`);
