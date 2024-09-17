817a61ff8acb469f2923142b454f75e376ef91b1
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-17 19:09:21 -0300

    Adiciona mensagem de boas-vindas personalizada com nome

--- Curso1/Desafios3/contagmRegressiva.js
+++ Curso1/Desafios3/contagmRegressiva.js
@@ -9,3 +9,11 @@ while (numero >= 0) {
     console.log(numero);
     numero--;
 }
+
+
+var numero = prompt("Digite um número para a contagem progressiva:");
+var contador = 0;
+while (contador <= numero) {
+    console.log(contador);
+    contador++;
+}
