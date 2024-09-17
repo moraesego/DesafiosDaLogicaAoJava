466beac0e46ec7b944b00529bec205f8a53fa2e9
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-17 18:33:38 -0300

    Adiciona verificação de idade para habilitação

--- Curso1/Desafios1/boasVindas.js
+++ Curso1/Desafios1/boasVindas.js
@@ -5,3 +5,10 @@ var idade = 25;
 var numeroDeVendas = 50;
 
 var nome = prompt("Qual é o seu nome?");
+
+var idade = prompt("Qual é a sua idade?");
+if (idade >= 18) {
+    alert("Pode tirar a habilitação!");
+} else {
+    alert("Não pode tirar habilitação")
+}
