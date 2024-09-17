4b4d61fafc971dfe8271806445e6fad2396310c7
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-17 19:13:45 -0300

    Adiciona verificação de maior ou menor de idade

--- Curso1/Desafios4/boasVindasNome.js
+++ Curso1/Desafios4/boasVindasNome.js
@@ -1,5 +1,15 @@
 var nome = "Dyogenes";
 console.log(`Olá, ${nome}!`);
 
+
+var idade = prompt("Qual a sua idade?");
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.");
+}
+
 var linguagem = prompt("Qual a linguagem de programação que você mais gosta?");
 console.log(`Você gosta de ${linguagem}`);
+
+
