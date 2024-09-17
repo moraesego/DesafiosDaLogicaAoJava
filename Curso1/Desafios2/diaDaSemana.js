9a5df1c714d7d6fe976210cf372aab2ebdc26dca
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-17 18:38:53 -0300

    Adiciona verificação de dia da semana e saudação de fim ou meio de semana

---
+++ Curso1/Desafios2/diaDaSemana.js
@@ -0,0 +1,6 @@
+var diaDaSemana = prompt("Qual é o dia da semana?");
+if (diaDaSemana === "Sábado" || diaDaSemana === "Domingo") {
+    alert("Bom fim de semana!");
+} else {
+    alert("Boa semana!");
+}
