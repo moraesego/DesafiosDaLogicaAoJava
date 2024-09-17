1f363a19d22a5eac78202a23c8a77f00e8e22d4a
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-17 18:50:24 -0300

    Adiciona contador regressivo de 10 a 0 usando loop while

---
+++ Curso1/Desafios3/Contador.js
@@ -0,0 +1,11 @@
+var contador = 1;
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
+
+var contador = 10;
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
