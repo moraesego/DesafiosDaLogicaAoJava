a6cc66de7a6f5d449d8ec3191787ecaac5250558
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-18 07:35:54 -0300

    feat: adiciona cálculo de desconto na classe CalcularValorTotal

--- Curso3/Desafios2/CalcularValorTotal.java
+++ Curso3/Desafios2/CalcularValorTotal.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class CalcularValorTotal {
+    public static void main(String[] args) {
+        double precoProduto = 19.99;
+        int quantidade = 3;
+        double valorTotal = precoProduto * quantidade;
+
+        double percentualDesconto = 10; // 10%
+        double valorDesconto = (valorTotal * percentualDesconto) / 100;
+        double valorFinal = valorTotal - valorDesconto;
+
+        System.out.println("O valor total com desconto é: R$ " + valorFinal);
+    }
 }
