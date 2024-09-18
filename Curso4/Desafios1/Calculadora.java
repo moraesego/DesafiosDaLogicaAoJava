f791421fab83018986ccd44aac37998914daf58e
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-18 16:22:37 -0300

    feat: adiciona classe Calculadora com método dobra e instancia no Main

---
+++ Curso4/Desafios1/Calculadora.java
@@ -0,0 +1,7 @@
+public class Calculadora {
+    int numero;
+
+    void dobra(){
+        System.out.println( "o Dobro do seu número ´" + (numero * 2));
+    }
+}
--- Curso4/Desafios1/Main.java
+++ Curso4/Desafios1/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
         pessoa.saudacao();
+
+        Calculadora dobro = new Calculadora();
+
+        dobro.numero = 5;
+
+        dobro.dobra();
     }
 }
