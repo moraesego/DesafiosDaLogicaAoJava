95a4586d5e62709761b68c3c3c8739f142a9c7c4
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-21 15:56:00 -0300

    Add MainDesafios2 com exemplo de uso de IdadePessoa

---
+++ Curso4/Desafios2/MainDesafios2.java
@@ -0,0 +1,14 @@
+import desafio2.modelos.idadePessoa;
+import desafio2.modelos.ContaBancaria;
+
+public class MainDesafios2 {
+    public static void main(String[] args) {
+        idadePessoa pessoa1 = new idadePessoa();
+        pessoa1.setNome("Pablo");
+        pessoa1.setIdade(20);
+
+        pessoa1.verificarIdade();
+
+
+    }
+}
