a29f27a1690a9dde8b5f022644d3462ae7266a86
Dyogenes Moraes <dyogenes.moraes@souunit.com.br>
2024-09-21 16:30:27 -0300

    feat: atualização da classe  para exemplificar uso da classe Aluno

--- Curso4/Desafios2/MainDesafios2.java
+++ Curso4/Desafios2/MainDesafios2.java
@@ -1,5 +1,6 @@
 import desafio2.modelos.idadePessoa;
 import desafio2.modelos.ContaBancaria;
+import desafio2.modelos.Aluno;
 
 public class MainDesafios2 {
     public static void main(String[] args) {
@@ -8,7 +9,12 @@ public class MainDesafios2 {
         pessoa1.setIdade(20);
 
         pessoa1.verificarIdade();
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Pedrinho");
+        aluno1.setNotas(10, 20);
 
+
+        System.out.println("A média das notas de " + aluno1.getNome() + " é: " + aluno1.calcularMedia());
 
     }
 }
